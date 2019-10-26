a09c5324bee05ab4f210baa4ec3bc3dd62af5580
Takundei Gora <t.gora@jacobs-university.de>
2019-10-27 01:55:53 +0200

    init

--- light_power_scripts.js
+++ light_power_scripts.js
@@ -28,3 +28,6 @@ fetch('http://10.120.51.145:2630/api/light/state/on', requestOptions)
   .then(response => response.text())
   .then(result => console.log(result))
   .catch(error => console.log('error', error));
+
+
+  
